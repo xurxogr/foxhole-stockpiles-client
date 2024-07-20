d094901a76d86880b9a0dddc7a7f7d848c888ce5
Xurxogr <jgroca@gmail.com>
2024-07-20 11:56:58 +0200

    chore: Internal variable refactor

--- foxhole_stockpiles/ui/app.py
+++ foxhole_stockpiles/ui/app.py
@@ -24,15 +24,15 @@ class App(tb.Window):
 
         super().__init__(themename=theme, title=title, minsize=(width, height), resizable=(False, False))
 
-        self.__key_text = tb.StringVar()
-        self.__token_text = tb.StringVar()
-        self.__counter = 0
-        self.__capture_enabled = False
+        self._key_text = tb.StringVar()
+        self._token_text = tb.StringVar()
+        self._counter = 0
+        self._capture_enabled = False
 
         self.create_widgets()
 
         # Fill the components with the values from config.ini
-        self.__read_options()
+        self.read_options()
         self.mainloop()
 
 
@@ -42,11 +42,11 @@ class App(tb.Window):
 
         # Screenshot Key
         tb.Label(main_frame, text="Screenshot key:").grid(row=0, column=0, sticky=W, padx=(0, 5), pady=5)
-        tb.Entry(main_frame, textvariable=self.__key_text, state="disabled").grid(row=0, column=1, sticky=EW, pady=5)
+        tb.Entry(main_frame, textvariable=self._key_text, state="disabled").grid(row=0, column=1, sticky=EW, pady=5)
 
         # Server Token
         tb.Label(main_frame, text="Server token:").grid(row=1, column=0, sticky=W, padx=(0, 5), pady=5)
-        tb.Entry(main_frame, textvariable=self.__token_text).grid(row=1, column=1, sticky=EW, pady=5)
+        tb.Entry(main_frame, textvariable=self._token_text).grid(row=1, column=1, sticky=EW, pady=5)
 
         # Buttons
         button_frame = tb.Frame(main_frame)
@@ -67,23 +67,23 @@ class App(tb.Window):
         main_frame.rowconfigure(4, weight=1)
         main_frame.columnconfigure(1, weight=1)
 
-        self.__text_area = tb.Text(text_frame, wrap=WORD, height=10)
-        self.__text_area.pack(side=LEFT, fill=BOTH, expand=YES)
+        self._text_area = tb.Text(text_frame, wrap=WORD, height=10)
+        self._text_area.pack(side=LEFT, fill=BOTH, expand=YES)
 
-        scrollbar = tb.Scrollbar(text_frame, orient=VERTICAL, command=self.__text_area.yview)
+        scrollbar = tb.Scrollbar(text_frame, orient=VERTICAL, command=self._text_area.yview)
         scrollbar.pack(side=RIGHT, fill=Y)
 
-        self.__text_area.configure(yscrollcommand=scrollbar.set)
+        self._text_area.configure(yscrollcommand=scrollbar.set)
 
     def change_key(self):
         """
         "Change keybind" callback.
         Opens a new thread to capture a new keybind.
         """
-        self.__key_text.set("Waiting for a new key...")
-        threading.Thread(target=self.__read_key).start()
+        self._key_text.set("Waiting for a new key...")
+        threading.Thread(target=self.read_key).start()
 
-    def __read_key(self):
+    def read_key(self):
         """
         Waits for a new combinations of key pressed and stores it in the appropriate entry
         """
@@ -93,26 +93,26 @@ class App(tb.Window):
         if key is None:
             return
 
-        self.__set_hotkey(key=key)
+        self.set_hotkey(key=key)
 
-    def __set_hotkey(self, key: str):
+    def set_hotkey(self, key: str):
         """
         Updates the UI with the defined keybind.
         If there is keybind or it's invalid to be used as global hotkey a message will be displayes
         """
         if not key:
-            self.__hotkey = None
-            self.__key_text.set('<No key defined>')
+            self._hotkey = None
+            self._key_text.set('<No key defined>')
 
         try:
             k = KeyPress()
-            self.__hotkey = k.prepare_for_global_hotkey(key)
-            self.__key_text.set(key)
+            self._hotkey = k.prepare_for_global_hotkey(key)
+            self._key_text.set(key)
         except ValueError:
-            self.__hotkey = None
-            self.__key_text.set('invalid key detected: {}'.format(key))
+            self._hotkey = None
+            self._key_text.set('invalid key detected: {}'.format(key))
 
-    def __read_options(self):
+    def read_options(self):
         """
         Read the config.ini file options and update the appropriate UI fields
         """
@@ -122,59 +122,59 @@ class App(tb.Window):
         config_token = settings.get(Settings.SECTION_SERVER, Settings.OPTION_TOKEN)
         config_key = settings.get(Settings.SECTION_KEYBIND, Settings.OPTION_KEY)
 
-        self.__set_hotkey(key=config_key)
-        self.__token_text.set(config_token)
+        self.set_hotkey(key=config_key)
+        self._token_text.set(config_token)
 
     def save_options(self):
         """
         Save the options to config.ini
         """
         settings = Settings()
-        settings.set(section=Settings.SECTION_SERVER, option=Settings.OPTION_TOKEN, value=self.__token_text.get())
-        settings.set(section=Settings.SECTION_KEYBIND, option=Settings.OPTION_KEY, value=self.__key_text.get())
+        settings.set(section=Settings.SECTION_SERVER, option=Settings.OPTION_TOKEN, value=self._token_text.get())
+        settings.set(section=Settings.SECTION_KEYBIND, option=Settings.OPTION_KEY, value=self._key_text.get())
         settings.save()
 
     def capture(self):
         """
         "Enable capture" callback. Used to enable or disable the global keypress to take screenshots of Foxhole
         """
-        if self.__capture_enabled:
+        if self._capture_enabled:
             self.capture_button.configure(text="Start Capture", bootstyle=LIGHT)
-            self.__print("Capture is disabled.")
-            if self.__thread:
-                self.__thread.stop()
-                self.__thread = None
-        elif self.__hotkey: # Enable the capture if the hotkey is set
-            self.__print("Capture is now enabled.")
+            self.message("Capture is disabled.")
+            if self._thread:
+                self._thread.stop()
+                self._thread = None
+        elif self._hotkey: # Enable the capture if the hotkey is set
+            self.message("Capture is now enabled.")
             self.capture_button.configure(text="Stop Capture", bootstyle=DANGER)
-            self.__thread = keyboard.GlobalHotKeys({self.__hotkey: self.screenshot})
-            self.__thread.start()
+            self._thread = keyboard.GlobalHotKeys({self._hotkey: self.screenshot})
+            self._thread.start()
 
 
-        self.__capture_enabled = not self.__capture_enabled
+        self._capture_enabled = not self._capture_enabled
 
     def screenshot(self):
-        img = self.__screenshot()
+        img = self._screenshot()
         if not img:
             return
 
         # Open a new thread to avoid blocking the execution
-        threading.Thread(target=self.__send_image, args=(img,)).start()
+        threading.Thread(target=self.send_image, args=(img,)).start()
 
-    def __send_image(self, img):
+    def send_image(self, img):
         """
         Sends an image to foxhole_stockpiles server
         :param img: Image to send
         """
-        self.__counter += 1
-        current_screenshot = self.__counter
-        self.__print(message="[{}] Sending screenshot...".format(current_screenshot))
+        self._counter += 1
+        current_screenshot = self._counter
+        self.message(message="[{}] Sending screenshot...".format(current_screenshot))
         byte_io = BytesIO()
         img.save(byte_io, 'png')
         byte_io.seek(0)
 
         timeout = Timeout(10.0, read=30.0)
-        headers = {"API_KEY": self.__token_text.get()}
+        headers = {"API_KEY": self._token_text.get()}
         with Client(headers=headers, verify=False, timeout=timeout) as client:
             try:
                 response = client.post(
@@ -182,7 +182,7 @@ class App(tb.Window):
                     files={'image': ('screenshot.png', byte_io, 'image/png')}
                 )
             except Exception as ex:
-                self.__print(message="[{}] Error sending the image. {}".format(current_screenshot, str(ex)))
+                self.message(message="[{}] Error sending the image. {}".format(current_screenshot, str(ex)))
             else:
                 try:
                     text = response.json().get('message')
@@ -190,35 +190,32 @@ class App(tb.Window):
                     text = response.text
 
                 if response.status_code == 200:
-                    self.__print(message="[{}] {}".format(current_screenshot, text))
+                    self.message(message="[{}] {}".format(current_screenshot, text))
                 else:
-                    self.__print(message="[{}] Error sending the image. Status_code: {}. Error: {}".format(current_screenshot, response.status_code, text))
+                    self.message(message="[{}] Error sending the image. Status_code: {}. Error: {}".format(current_screenshot, response.status_code, text))
 
-    def __screenshot(self):
+    def _screenshot(self):
         """
         Take an screeshot of Foxhole
         """
         try:
             foxhole = pywinctl.getWindowsWithTitle(title="War", condition=pywinctl.Re.STARTSWITH)[0]
         except Exception:
-            self.__print(message="Foxhole is not running")
+            self.message(message="Foxhole is not running")
             return None
 
         if foxhole.isMinimized:
-            self.__print(message="Foxhole is minimized")
+            self.message(message="Foxhole is minimized")
             return None
 
         if not foxhole.isActive:
-            self.__print(message="Foxhole should be the active window")
+            self.message(message="Foxhole should be the active window")
             return None
 
         region=foxhole.getClientFrame()
         return ImageGrab.grab(bbox=region, all_screens=True)
 
-    def __print(self, message: str):
-        self.__add_message(message)
-
-    def __add_message(self, message: str):
+    def message(self, message: str):
         current_time = datetime.now().strftime("%H:%M:%S")
-        self.__text_area.insert(END, "[{}] {}\n".format(current_time, message))
-        self.__text_area.see(END)
+        self._text_area.insert(END, "[{}] {}\n".format(current_time, message))
+        self._text_area.see(END)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from foxhole_stockpiles.ui.app import App
 
 
-window = App(title="Foxhole stockpiles v0.3", width=700, height=400)
+window = App(title="Foxhole stockpiles v0.3.1", width=700, height=400)
 
 window.mainloop()
