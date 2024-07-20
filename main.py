fe22a177314a1f9d75d4766285dd79942bb565e2
Xurxogr <jgroca@gmail.com>
2024-07-20 11:28:15 +0200

    revert d7794c98325e95f as it didn't helped to solve the problem of the user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from foxhole_stockpiles.ui.app import App
 
 
-window = App(title="Foxhole stockpiles", width=700, height=400)
+window = App(title="Foxhole stockpiles v0.2.1", width=700, height=400)
 
 window.mainloop()
