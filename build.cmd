056c873f447e5cd1cb8f0551898ce21bbe1cc22e
Xurxogr <jgroca@gmail.com>
2025-10-19 12:43:10 +0200

    chore: change the name of the executable

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 if "%VIRTUAL_ENV%" == "" (
     echo "Command needs to be executed in a virtual env"
 ) else (
-    pyinstaller --name "foxhole_stockpiles" --noconfirm --onefile --windowed --add-data "%~dp0/foxhole_stockpiles;foxhole_stockpiles/" "%~dp0/foxhole_stockpiles/main.py"
+    pyinstaller --name "fs_client" --noconfirm --onefile --windowed --add-data "%~dp0/foxhole_stockpiles;foxhole_stockpiles/" "%~dp0/foxhole_stockpiles/main.py"
 )
